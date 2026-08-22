2bcdfd8a1184d22faac023061269613e5d2863fc
Fábio Machado de Oliveira <machado2@gmail.com>
2026-08-22 14:01:53 -0300

    fix: set responsive viewport globally

--- web/src/pages/_app.tsx
+++ web/src/pages/_app.tsx
@@ -3,23 +3,29 @@ import type { AppProps } from 'next/app'
 import Layout from '@/components/Layout'
 import { SessionProvider } from "next-auth/react"
 import { MDXProvider } from '@mdx-js/react'
+import Head from 'next/head'
 
 import { GlobalLanguageProvider } from '@/components/GlobalLanguage'
 
 export default function App({ Component, pageProps: { session, ...pageProps } }: AppProps) {
   return (
-    <SessionProvider session={session}>
-      <GlobalLanguageProvider>
-        <MDXProvider>
-          {pageProps.hideHeader ? (
-            <Component {...pageProps} />
-          ) : (
-            <Layout>
+    <>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
+      <SessionProvider session={session}>
+        <GlobalLanguageProvider>
+          <MDXProvider>
+            {pageProps.hideHeader ? (
               <Component {...pageProps} />
-            </Layout>
-          )}
-        </MDXProvider>
-      </GlobalLanguageProvider>
-    </SessionProvider>
+            ) : (
+              <Layout>
+                <Component {...pageProps} />
+              </Layout>
+            )}
+          </MDXProvider>
+        </GlobalLanguageProvider>
+      </SessionProvider>
+    </>
   );
 }
